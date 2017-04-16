82f2697c0b774efa60420f262197541361e2c814
Denisplusplus <whinslow@mail.ru>
2017-04-16 16:41:57 +0300

    Update functions.c

--- Code/functions.c
+++ Code/functions.c
@@ -4,20 +4,80 @@
 #include "functions.h"
 #include "matrix_circuit.h"
 
-
-void matrix_inside_display(Node* matrix)
+int non_zero_str(int row, char* filename, int rows, int columns)
 {
-	while (matrix) {
-		printf("%d ", matrix->rows_id);
-		printf("%d ", matrix->value);
-		
-		printf("\n");
-		matrix=matrix->next;
+	FILE *file = fopen(filename, "r");
+	int element = 0;
+	int q = 0; //quantity of zero elements
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < columns; j++) {
+			fscanf(file, "%d", &element);
+			if ((i == row + 1) && (element == 0)) {
+				q++;
+			}
+		}
 	}
+	return !(q == rows);
+	fclose(file);
 }
 
 
-void add(Node **matrix, int data, int column)
+int columns_quantity(char* filename)
+{
+	FILE *file = fopen(filename, "r");
+	char c;
+    int columns = 1; // because the number of columns bigger than amount of spaces in 1;
+
+    while ((c = fgetc(file)) !='\n') {
+        if (isspace(c)) {
+            columns++;
+        }
+    }
+	return(columns);
+	fclose(file);
+}
+
+
+int rows_quantity(char* filename)
+{	
+	FILE *file = fopen(filename, "r");
+	int rows = 0;
+    while (! feof(file)) {
+        fscanf(file, "%*[^\n]%*c");
+        rows++;
+    }
+
+	return(rows);
+	fclose(file);
+}
+
+
+void information_about_representation()
+{
+	printf("Two cells correspond to a nonzero element: the first\n");
+	printf("contains the column number, the second contains the value\n"); 
+	printf("of the element. A zero in the first cell indicates the end of the\n");
+	printf("line, and the second cell in this case contains the number of the\n"); 
+	printf("next stored line. Zeros in both cells are a sign of the end of the\n");
+	printf("list of non-zero elements \n");
+}
+
+
+void matrix_inside_display(Node* matrix)
+{	
+	
+	printf("Column/Row  Value \n");
+	while (matrix) {
+		printf("     %d ", matrix->coloumns_id);
+		printf("       %d\n", matrix->value);
+		matrix=matrix->next;
+	}
+	printf("Type: program -repinfo to gain more information about matrix representation\n");
+
+}
+
+
+void add(Node **matrix, int column, int data)
 {
     Node *temp = NULL; 
     if(!(*matrix))
@@ -25,11 +85,11 @@ void add(Node **matrix, int data, int column)
         temp = (Node *)malloc(sizeof(Node));
         temp->next = NULL;
         temp->value = data;
-        temp->rows_id = column;
+        temp->coloumns_id = column;
         *matrix = temp;
         return;
     }
-    add(&(*matrix)->next, data, column);
+    add(&(*matrix)->next, column, data);
     free(temp);
 } 
 
@@ -45,26 +105,9 @@ Node* matrix_read(int argc, char **argv) {
         printf("I/O Error: can't open file.\n");
         exit(2);
     }
-    // count the rows
-    int rows = 0;
-    while (! feof(in)) {
-        fscanf(in, "%*[^\n]%*c");
-        rows++;
-    }
-
-    rewind(in);
     
-    char c;
-    int columns = 1; // because the number of columns bigger than amount of spaces in 1;
-
-    while ((c = fgetc(in)) !='\n') {
-        if (isspace(c)) {
-            columns++;
-       }
-    }
-    
-    rewind(in);
-    
+    int rows = rows_quantity(argv[1]);
+    int columns = columns_quantity(argv[1]);
     // read matrix from file
     int element = 0;
     Node *matrix;
@@ -72,20 +115,19 @@ Node* matrix_read(int argc, char **argv) {
     for (int row = 0; row < rows; row++) {
     	for (int column = 0; column <columns; column++) {
     		fscanf(in, "%d", &element);
-    		if (column == 0) {
-    			add(&matrix, (row + 1), 0);
-    		
+    		if (column == 0 && non_zero_str(row, argv[1], rows, columns)) {
+    			add(&matrix, 0, (row + 1));
     		}
     		if (element != 0) { 
-    		    add(&matrix, element, (column+1));
+    		    add(&matrix, (column+1), element);
+			}
+			if ((row == rows - 1) && (column == columns - 1)) {
+				add(&matrix, 0, 0);
 			}	
                         
     	}	
     }
  	return(matrix);
-    
-  
     fclose(in);
-    
 }
 
