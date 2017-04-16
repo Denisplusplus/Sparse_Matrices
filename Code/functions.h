c33ec4e2e956f53040c67ae66c8524beb9e07850
Denisplusplus <whinslow@mail.ru>
2017-04-16 16:42:26 +0300

    Update functions.h

--- Code/functions.h
+++ Code/functions.h
@@ -1,6 +1,15 @@
 #ifndef _FUNCTIONS_H_
 #define _FUNCTIONS_H_
 
+#include "matrix_circuit.h"
 
-void matrix_read(int argc, char **argv);
+
+Node* matrix_read(int argc, char **argv);
+void matrix_inside_display(Node* matrix);
+void information_about_representation();
+void add(Node **matrix, int data, int row);
+int non_zero_str(int row, char* filename, int rows, int columns);
+int columns_quantity(char* filename);
+int rows_quantity(char* filename);
+
 #endif
