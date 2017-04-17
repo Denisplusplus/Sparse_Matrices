4dd33ca09a6d6095c3ac0f6e784205a186074443
Denisplusplus <whinslow@mail.ru>
2017-04-17 18:47:44 +0300

    Update functions.h

--- Code/functions.h
+++ Code/functions.h
@@ -5,6 +5,7 @@
 
 
 Node* matrix_read(int argc, char **argv);
+Node* matrix_devide(Node **matrix);
 void matrix_display_internal(Node* matrix);
 void matrix_display_external(Node* matrix, int rows, int columns);
 int element_find(Node* matrix, int i, int j);
@@ -13,5 +14,8 @@ void add(Node **matrix, int data, int row);
 int non_zero_str(int row, char* filename, int rows, int columns);
 int columns_quantity(char* filename);
 int rows_quantity(char* filename);
+int element_max_find(Node *matrix);
+int column_penult_element(Node* matrix);
+int column_max(Node* matrix);
 
 #endif
