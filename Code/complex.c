2db34012df542fa0c535a35be2bb9eb838bef405
Denisplusplus <whinslow@mail.ru>
2017-05-03 19:29:53 +0300

    Update complex.c

--- Code/complex.c
+++ Code/complex.c
@@ -6,28 +6,28 @@
 
 double complex_abs(complex x)
 {
-     return (hypot(x.a, x.b));
+     return (hypot(x.re, x.im));
 }
 
 complex complex_divide(complex x, complex y)
 {
 	complex t;
 
-	t.a = (x.a * y.a + x.b * y.b) / (y.a * y.a + y.b * y.b);
-	t.b = (y.a * x.b - x.a * y.b) / (y.a * y.a + y.b * y.b);
+	t.re = (x.re * y.re + x.im * y.im) / (y.re * y.re + y.im * y.im);
+	t.im = (y.re * x.im - x.re * y.im) / (y.re * y.re + y.im * y.im);
 
 	return t;
 }
 
 void complex_print(complex x)
 {	
-	if (x.b == 0.0) {
-		printf("%.1lf\n", x.a);
+	if (x.im == 0.0) {
+		printf("%.1lf\n", x.re);
 	} else
-	if (x.b < 0.0) {
-		printf("%.1lf%.1lfi\n", x.a, x.b);
+	if (x.im < 0.0) {
+		printf("%.1lf%.1lfi\n", x.re, x.im);
 	} else {
-		printf("%.1lf+%.1lfi\n", x.a, x.b);
+		printf("%.1lf+%.1lfi\n", x.re, x.im);
 	} 
 
 	
@@ -35,7 +35,7 @@ void complex_print(complex x)
 
 int element_not_zero(complex x)
 {
-	if ((x.a != 0.0) && (x.b != 0.0)) {
+	if ((x.re != 0.0) && (x.im != 0.0)) {
 		return 1;
 	}
 	return 0;
@@ -45,15 +45,25 @@ int element_not_zero(complex x)
 complex element_zero()
 {
 	complex x;
-	x.a = 0.0;
-	x.b = 0.0;
+	x.re = 0.0;
+	x.im = 0.0;
 	return(x);
 }
 
 complex complexing(int row)
 {
 	complex x;
-	x.a = (double)(row);
-	x.b = 0.0;
+	x.re = (double)(row);
+	x.im = 0.0;
 	return x;
+}
+
+int equal(complex x, complex y)
+{
+	if ((x.re == y.re) && (x.im == y.im)) {
+		return 1;
+	}
+	return 0;
+
+
 }
